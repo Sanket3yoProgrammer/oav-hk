b726a2e9a4b0465face30d06f02c6aae6c46e622
Sanket Kumar Padhan <152056082+Sanket3yoProgrammer@users.noreply.github.com>
2024-11-17 15:59:53 +0530

    Update AboutSection.tsx

--- src/components/about/AboutSection.tsx
+++ src/components/about/AboutSection.tsx
@@ -25,7 +25,7 @@ export default function AboutSection() {
         <div className="mx-auto max-w-2xl lg:text-center">
           <h2 className="text-base font-semibold leading-7 text-blue-600">About Us</h2>
           <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl dark:text-white">
-            Excellence in Education Since 1970
+            Excellence in Education Since 2020
           </p>
           <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-200">
             Odisha Adarsha Vidyalaya, H. Katapali is committed to providing comprehensive education
